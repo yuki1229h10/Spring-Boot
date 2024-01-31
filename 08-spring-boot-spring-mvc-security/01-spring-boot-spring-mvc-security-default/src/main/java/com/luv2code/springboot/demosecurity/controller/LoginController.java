5e5cdeb1002aca791b5a4f433b750db4b151556a
yukiroom12 <zenba.y@room12.jp>
2024-01-31 17:04:24 +0900

    create form for username and password

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * ログイン関連の制御を処理する
+ * @author Yuki
+ */
+@Controller
+public class LoginController {
+	
+	/**
+	 * ログインページを表示する
+	 * @return plain-login.htmlを表示する
+	 */
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+		return "plain-login";
+	}
+}
