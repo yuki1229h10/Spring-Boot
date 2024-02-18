8b528f9cdb6b85d7ce8ac6b5a76e96da56f74ce4
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-02-18 22:35:32 +0900

    use entity manager

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,29 @@
+package com.luv2code.cruddemo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.cruddemo.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+	// define field for entity manager
+	private EntityManager entityManager;
+
+	// inject entity manager using constructor injection
+	@Autowired
+	public StudentDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	// implement save method
+	@Override
+	@Transactional
+	public void save(Student theStudent) {
+		entityManager.persist(theStudent);
+	}
+}
