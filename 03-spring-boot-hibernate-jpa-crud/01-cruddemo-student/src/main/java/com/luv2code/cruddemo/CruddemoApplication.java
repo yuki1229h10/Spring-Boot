17ccf85ab8129431e9686f3046e5069919d60d97
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-02-21 00:12:49 +0900

    create multiple users into db

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -28,11 +28,16 @@ public class CruddemoApplication {
 	private void createMultipleStudents(StudentDAO studentDAO) {
 
 		// create multiple students
-		System.out.println("Creating 3 student object...");
+		System.out.println("Creating 3 student objects ...");
 		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
 
 		// save the student objects
-
+		System.out.println("Saving the studenst ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
