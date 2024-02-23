63101ba5f3ad905e94e077ade8526931c37ed6b3
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-02-23 20:13:38 +0900

    create queryForStudentsByLastName

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -38,7 +38,19 @@ public class StudentDAOImpl implements StudentDAO {
 	@Override
 	public List<Student> findAll() {
 		// create query
-		TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName asc", Student.class);
+		TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+		// return query results
+		return theQuery.getResultList();
+	}
+
+	@Override
+	public List<Student> findByLastName(String theLastName) {
+		// create query
+		TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+		// set query parameters
+		theQuery.setParameter("theData", theLastName);
 
 		// return query results
 		return theQuery.getResultList();
