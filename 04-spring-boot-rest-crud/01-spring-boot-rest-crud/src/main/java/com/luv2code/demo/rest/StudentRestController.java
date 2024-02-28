bea0e3fe221b3f249e80cbf2cf1f72a7b6fcf4fb
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-02-29 00:00:01 +0900

    test

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -9,20 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.demo.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+
+	private List<Student> theStudents;
+
+	// define @PostConstruct to load the student data ... only once!
+
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
+
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
 
 	// define end point for "/students" - return a list of students
 
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-
-		List<Student> theStudents = new ArrayList<>();
-
-		theStudents.add(new Student("Poornima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));
 
 		return theStudents;
 	}
