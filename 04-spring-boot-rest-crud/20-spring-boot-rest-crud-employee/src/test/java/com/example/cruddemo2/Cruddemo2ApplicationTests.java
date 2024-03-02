8dcefddb1ceeed2305b2b14fb19ea959156887c7
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-02 15:53:15 +0900

    add some files

---
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/test/java/com/example/cruddemo2/Cruddemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruddemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cruddemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
