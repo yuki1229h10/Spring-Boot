c5021f9977cf1abbb636a8cadd35baf22d7c968b
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-03 17:36:47 +0900

    create findById,save,deleteById

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/dao/EmployeeDAO.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/dao/EmployeeDAO.java
@@ -7,4 +7,10 @@ import com.example.cruddemo2.entity.Employee;
 public interface EmployeeDAO {
 
 	List<Employee> findAll();
+
+	Employee findById(int theId);
+
+	Employee save(Employee theEmployee);
+
+	void deleteById(int theId);
 }
