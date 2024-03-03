73944682b85fbdc59ab0c3c3a22cd550d54d6194
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-03 18:32:54 +0900

    create findById,save,deleteById

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.cruddemo2.dao.EmployeeDAO;
 import com.example.cruddemo2.entity.Employee;
@@ -21,6 +22,23 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	public List<Employee> findAll() {
 		return employeeDAO.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		return employeeDAO.findById(theId);
+	}
+
+	@Transactional
+	@Override
+	public Employee save(Employee theEmployee) {
+		return employeeDAO.save(theEmployee);
+	}
+
+	@Transactional
+	@Override
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
 	}
 
 }
