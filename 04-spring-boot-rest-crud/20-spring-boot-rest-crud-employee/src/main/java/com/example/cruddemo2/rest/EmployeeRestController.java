54ad44a60c8cef24fa5bcac68df0051d2cac539a
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-04 00:21:49 +0900

    create addEmployee

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +42,19 @@ public class EmployeeRestController {
 		}
 
 		return theEmployee;
+	}
+
+	// add mapping for POST /employees - add new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+		// also just in case they pass an id in JSON ... set id to 0
+		// this is to force a save of new item ... instead of update
+
+		theEmployee.setId(0);
+
+		Employee dbEmployee = employeeService.save(theEmployee);
+
+		return dbEmployee;
 	}
 }
