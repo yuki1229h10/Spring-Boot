f43d951156af6da07c8e6e3f66a19e4c51f9bcaa
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-04 23:56:11 +0900

    deleteEmployee

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo2/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.example.cruddemo2.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,22 @@ public class EmployeeRestController {
 
 		return dbEmployee;
 
+	}
+
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+
+		Employee tempEmployee = employeeService.findById(employeeId);
+
+		// throw exception if null
+
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		employeeService.deleteById(employeeId);
+
+		return "Deleted employee id - " + employeeId;
 	}
 }
