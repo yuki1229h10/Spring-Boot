5c178b053f8684ee7608d2a48934f051363fff9a
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-06 23:18:54 +0900

    use data rest

---
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo2/Cruddemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.cruddemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cruddemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cruddemo2Application.class, args);
+	}
+
+}
