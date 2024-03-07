e0c4a9ed0fc097aba2307fc31681936f17fbc731
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-07 22:31:12 +0900

    add property for rest

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo2/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/cruddemo2/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.example.cruddemo2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.cruddemo2.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
