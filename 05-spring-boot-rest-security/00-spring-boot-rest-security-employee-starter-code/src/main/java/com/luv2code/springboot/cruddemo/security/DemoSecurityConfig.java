847583da313d1b796c1d41a1d6b82049bb71a5a2
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-08 22:22:51 +0900

    still writing SecurityChain

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,5 +35,12 @@ public class DemoSecurityConfig {
 
 		return new InMemoryUserDetailsManager(john, mary, susan);
 
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(
+				configurer -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE"));
 	}
 }
