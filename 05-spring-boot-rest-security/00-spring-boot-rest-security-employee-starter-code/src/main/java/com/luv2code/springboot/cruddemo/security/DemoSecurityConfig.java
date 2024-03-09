f06d33a93efbe4762fd35a2747aa7992788d132c
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-09 11:59:31 +0900

    SecurityChain

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -41,6 +42,19 @@ public class DemoSecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests(
-				configurer -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE"));
+				configurer -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+						.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+						.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+						.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+						.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+		// use HTTP Basic authentication
+		http.httpBasic(Customizer.withDefaults());
+
+		// disable Cross Site Request Forgery (CSRF)
+		// in general, not required for state less REST APIs that use POST, PUT, DELETE and/or PATCH
+		http.csrf(csrf -> csrf.disable());
+
+		return http.build();
 	}
 }
