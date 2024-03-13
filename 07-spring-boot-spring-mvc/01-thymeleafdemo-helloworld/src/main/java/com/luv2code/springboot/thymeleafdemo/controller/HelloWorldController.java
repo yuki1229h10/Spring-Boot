0b17c625da51914c08a3b942897c0a8894b423fa
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-14 00:19:40 +0900

    @RequestParam

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -37,6 +38,21 @@ public class HelloWorldController {
 
 		// create the message
 		String result = "Yo! " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
+		return "helloworld";
+	}
+
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThreee(@RequestParam("studentName") String theName, Model model) {
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
 
 		// add message to the model
 		model.addAttribute("message", result);
