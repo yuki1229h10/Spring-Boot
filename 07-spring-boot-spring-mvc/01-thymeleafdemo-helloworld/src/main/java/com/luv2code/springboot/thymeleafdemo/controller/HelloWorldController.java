3f2d07a2120809dd192cf6a84d501c640ea06a37
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-14 00:08:04 +0900

    create letsShoutDude

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -17,6 +20,27 @@ public class HelloWorldController {
 
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+
+	// need a controller method to read form data and
+	// add data to the model
+
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+
+		// create the message
+		String result = "Yo! " + theName;
+
+		// add message to the model
+		model.addAttribute("message", result);
+
 		return "helloworld";
 	}
 
