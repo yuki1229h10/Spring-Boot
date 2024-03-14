3eff89ef5d843fa0834ab3fce5588475ebadfff2
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-14 19:42:28 +0900

    PostMapping

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,7 +47,7 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 
-	@RequestMapping("/processFormVersionThree")
+	@PostMapping("/processFormVersionThree")
 	public String processFormVersionThreee(@RequestParam("studentName") String theName, Model model) {
 
 		// convert the data to all caps
