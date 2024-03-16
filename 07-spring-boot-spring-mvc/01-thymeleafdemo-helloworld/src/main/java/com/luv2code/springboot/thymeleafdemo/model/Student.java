ee03eac5d0c83ca79d5afd66bf63020285123a03
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-16 15:55:36 +0900

    add check box

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,11 +1,14 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
 	private String country;
 	private String favoriteLanguage;
+	private List<String> favoriteSystems;
 
 	public Student() {
 
@@ -41,6 +44,14 @@ public class Student {
 
 	public void setFavoriteLanguage(String favoriteLanguage) {
 		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public List<String> getFavoriteSystems() {
+		return favoriteSystems;
+	}
+
+	public void setFavoriteSystems(List<String> favoriteSystems) {
+		this.favoriteSystems = favoriteSystems;
 	}
 
 }
