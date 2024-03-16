b6412b26fcb0b3512f892bc05f4a24c7cb2a8389
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-16 23:08:36 +0900

    trim form whitespace

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,9 +1,12 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,17 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+	// add an initbinder ... to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@GetMapping("/")
 	public String showForm(Model theModel) {
@@ -22,6 +36,8 @@ public class CustomerController {
 
 	@PostMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
