34a45304b66856b36b770b680df25c4e786a3644
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-16 11:16:24 +0900

    add country list for select tag

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,9 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 @Controller
 public class StudentController {
 
+	@Value("${countries}")
+	private List<String> countries;
+
 	@GetMapping("/showStudentForm")
 	public String showForm(Model theModel) {
 
@@ -19,6 +25,9 @@ public class StudentController {
 
 		// add student object to the model
 		theModel.addAttribute("student", theStudent);
+
+		// add the list of countries to the model
+		theModel.addAttribute("countries", countries);
 
 		return "student-form";
 	}
