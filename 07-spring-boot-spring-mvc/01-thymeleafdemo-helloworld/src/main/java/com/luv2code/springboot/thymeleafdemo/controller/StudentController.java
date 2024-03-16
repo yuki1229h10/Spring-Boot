716e83c7185e78ee78b0a15c01299d39d426e4c5
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-16 16:04:23 +0900

    add system list for check box

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -20,6 +20,9 @@ public class StudentController {
 	@Value("${languages}")
 	private List<String> languages;
 
+	@Value("${systems}")
+	private List<String> systems;
+
 	@GetMapping("/showStudentForm")
 	public String showForm(Model theModel) {
 
@@ -34,6 +37,9 @@ public class StudentController {
 
 		// add the list of language to the model
 		theModel.addAttribute("languages", languages);
+
+		// add the list of systems to the model
+		theModel.addAttribute("systems", systems);
 
 		return "student-form";
 	}
