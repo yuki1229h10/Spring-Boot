635db1124c040b9c13a92cf83ced6271a502f018
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-26 00:07:02 +0900

    create App dao

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,26 @@
+package com.luv2code.cruddemo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luv2code.cruddemo.entity.Instructor;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+	// define field for entity manager
+	private EntityManager entityManager;
+
+	// inject entity manager using constructor injection
+	@Autowired
+	public AppDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public void save(Instructor theInstructor) {
+		entityManager.persist(theInstructor);
+	}
+}
