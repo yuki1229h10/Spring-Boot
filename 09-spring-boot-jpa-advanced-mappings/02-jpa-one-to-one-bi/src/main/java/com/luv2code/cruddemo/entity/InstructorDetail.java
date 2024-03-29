6f19fc2319146cca9ee2da740944a05b579485bc
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-03-29 23:58:17 +0900

    add OneToOne annotation

--- 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.luv2code.cruddemo.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +35,10 @@ public class InstructorDetail {
 
 	@Column(name = "hobby")
 	private String hobby;
+
+	// add @OneToOne annotation
+	@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	private Instructor instructor;
 
 	public InstructorDetail() {
 
@@ -65,6 +71,14 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	@Override
