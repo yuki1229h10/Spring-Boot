750dd9b08bbd3c3fe9c8d08df9618855d3b8af4d
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-04-10 23:39:20 +0900

    create deleteCourse

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -41,12 +41,25 @@ public class CruddemoApplication {
 
 			//	findInstructorWithCoursesJoinFetch(appDAO);
 
-			// updateInstructor(appDAO);
+			//  updateInstructor(appDAO);
 
 			//	updateCourse(appDAO);
 
-			deleteInstructor(appDAO);
+			//	deleteInstructor(appDAO);
+
+			deleteCourse(appDAO);
 		};
+	}
+
+	private void deleteCourse(AppDAO appDAO) {
+
+		int theId = 10;
+
+		System.out.println("Deleting course id: " + theId);
+
+		appDAO.deleteCourseById(theId);
+
+		System.out.println("Done!");
 	}
 
 	private void updateCourse(AppDAO appDAO) {
