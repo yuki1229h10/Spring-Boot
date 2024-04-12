de4061719cef1f7401d96c7dc7ccf1f3340fabee
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-04-12 23:59:13 +0900

    setup entity

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -23,31 +23,7 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			//	createInstructor(appDAO);
 
-			//	findInstructor(appDAO);
-
-			//	deleteInstructor(appDAO);
-
-			//	findInstructorDetail(appDAO);
-
-			//	deleteInstructorDetail(appDAO);
-
-			//	createInstructorWithCourses(appDAO);
-
-			//	findInstructorWithCourses(appDAO);
-
-			//	findCoursesForInstructor(appDAO);
-
-			//	findInstructorWithCoursesJoinFetch(appDAO);
-
-			//  updateInstructor(appDAO);
-
-			//	updateCourse(appDAO);
-
-			//	deleteInstructor(appDAO);
-
-			deleteCourse(appDAO);
 		};
 	}
 
