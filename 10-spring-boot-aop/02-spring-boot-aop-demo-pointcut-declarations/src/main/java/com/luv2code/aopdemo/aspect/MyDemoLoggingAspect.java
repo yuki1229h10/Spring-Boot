02a831109eba28cc74b7e0d28c8ecaac8d144a9a
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-05-05 20:18:32 +0900

    add convertAccountNamesToUpperCase method

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -28,6 +28,27 @@ public class MyDemoLoggingAspect {
 
 		// print out the results of the method call
 		System.out.println("\n=====>> result is: " + result);
+
+		// let's post-process the data ... let's modify it :-)
+
+		// convert the account names to uppercase
+		convertAccountNamesToUpperCase(result);
+
+		System.out.println("\n=====>> result is: " + result);
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+
+		// loop through accounts
+
+		for (Account tempAccount : result) {
+
+			// get uppercase version of name
+			String theUpperName = tempAccount.getName().toUpperCase();
+
+			// update the name on the account
+			tempAccount.setName(theUpperName);
+		}
 	}
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
