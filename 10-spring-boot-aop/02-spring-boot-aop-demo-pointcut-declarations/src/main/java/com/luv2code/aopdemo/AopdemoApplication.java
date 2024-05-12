f5be9ef819e5c9b67a04f649e932d8d73f039906
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-05-12 17:22:04 +0900

    Around Advice - Handle Exception - Coding

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -28,9 +28,24 @@ public class AopdemoApplication {
 			//	demoTheAfterReturningAdvice(theAccountDAO);
 			//	demoTheAfterThrowingAdvice(theAccountDAO);
 			//	demoTheAfterAdvice(theAccountDAO);
+			//	demoTheAroundAdvice(theTrafficFortuneService);
 
-			demoTheAroundAdvice(theTrafficFortuneService);
+			demoTheAroundAdviceHandleException(theTrafficFortuneService);
 		};
+	}
+
+	private void demoTheAroundAdviceHandleException(TrafficFortuneService theTrafficFortuneService) {
+
+		System.out.println("\nMain Program: demoTheAroundAdviceHandleException");
+
+		System.out.println("Caling getFortune()");
+
+		boolean tripWire = true;
+		String data = theTrafficFortuneService.getFortune(tripWire);
+
+		System.out.println("\nMy fortune is: " + data);
+
+		System.out.println("Finished");
 	}
 
 	private void demoTheAroundAdvice(TrafficFortuneService theTrafficFortuneService) {
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneService.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -3,4 +3,6 @@ package com.luv2code.aopdemo.service;
 public interface TrafficFortuneService {
 
 	String getFortune();
+
+	String getFortune(boolean tripWire);
 }
